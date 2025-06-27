efe6487e95d1e7779b546dc387299fe5f752afcd
joemmoma2022 <110416415+joemmoma2022@users.noreply.github.com>
2025-06-27 15:15:44 -0400

    Add files via upload

--- script/c19712931.lua
+++ script/c19712931.lua
@@ -19,43 +19,38 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()~=tp
 end
 
--- Filter: face-up opponent's monster
-function s.filter(c)
-	return c:IsFaceup() and c:IsControler(1-tp)
-end
-
+-- Target 1 monster your opponent controls
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,nil,tp,0,LOCATION_MZONE,1,1,nil)
 end
 
+-- Set up delayed destruction and damage at opponent's next End Phase
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if not tc or not tc:IsRelateToEffect(e) then return end
+	if not tc:IsRelateToEffect(e) then return end
 
-	-- Store Field ID to track the monster
-	local fid=tc:GetFieldID()
-	tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,2)
+	-- Store the turn count to track "next End Phase"
+	local turn_id=Duel.GetTurnCount()
 
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN,2)
+	e1:SetCountLimit(1)
 	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)
-		return Duel.GetTurnPlayer()==1-tp
+		return Duel.GetTurnPlayer()==1-tp and Duel.GetTurnCount()>turn_id
 	end)
 	e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
-		local g=Duel.GetMatchingGroup(function(c)
-			return c:GetFlagEffect(id)>0 and c:GetFieldID()==fid
-		end,tp,0,LOCATION_MZONE,nil)
-		local tc=g:GetFirst()
-		if tc then
+		local tc=e:GetLabelObject()
+		if tc and tc:IsRelateToEffect(e) and tc:IsControler(1-tp) and tc:IsOnField() then
 			Duel.Destroy(tc,REASON_EFFECT)
 			Duel.Damage(1-tp,1000,REASON_EFFECT)
 		end
 		e:Reset()
 	end)
+	e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN,2)
+	e1:SetLabelObject(tc)
 	Duel.RegisterEffect(e1,tp)
 end
