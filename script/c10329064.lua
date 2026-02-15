738d2ebf9185a0a00d1b41b0607b5fa944ee1b08
joemmoma2022 <110416415+joemmoma2022@users.noreply.github.com>
2026-02-15 17:16:05 -0300

    Update c10329064.lua

--- script/c10329064.lua
+++ script/c10329064.lua
@@ -1,89 +1,88 @@
---Binding Suppression Equip
 local s,id=GetID()
+local COUNTER_THREAD=0x8370
+
 function s.initial_effect(c)
-	--Activate and equip to opponent's monster
+	-- Enable Thread Counters
+	c:EnableCounterPermit(COUNTER_THREAD)
+
+	-- Activate: place 1 Thread Counter on all opponent's monsters
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetCategory(CATEGORY_COUNTER)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 
-	--Equip limit
+	-- Monsters with Thread Counter cannot attack
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_EQUIP_LIMIT)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetValue(s.eqlimit)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetTarget(s.atktg)
 	c:RegisterEffect(e2)
 
-	--Equipped monster cannot attack
+	-- Monsters with Thread Counter cannot change battle position
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_EQUIP)
-	e3:SetCode(EFFECT_CANNOT_ATTACK)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetTarget(s.atktg)
 	c:RegisterEffect(e3)
 
-	--Negate equipped monster's effects
+	-- Opponent's End Phase: burn 700 for each Thread Counter on field
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_EQUIP)
-	e4:SetCode(EFFECT_DISABLE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCondition(s.damcon)
+	e4:SetOperation(s.damop)
 	c:RegisterEffect(e4)
-	local e5=e4:Clone()
-	e5:SetCode(EFFECT_DISABLE_EFFECT)
+
+	-- When this card leaves field: remove all Thread Counters
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EVENT_LEAVE_FIELD)
+	e5:SetOperation(s.remop)
 	c:RegisterEffect(e5)
 
-	--Lose 500 ATK during each End Phase
+	-- If it leaves the field, banish it instead
 	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e6:SetCode(EVENT_PHASE+PHASE_END)
-	e6:SetRange(LOCATION_SZONE)
-	e6:SetOperation(s.atkop)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetValue(LOCATION_REMOVED)
 	c:RegisterEffect(e6)
-
-	--Banish when leaves the field
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_SINGLE)
-	e7:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-	e7:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e7:SetValue(LOCATION_REMOVED)
-	c:RegisterEffect(e7)
 end
 
---Target opponent's monster
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
-	if chk==0 then
-		return Duel.IsExistingTarget(aux.FaceupFilter(Card.IsControler,1-tp),tp,0,LOCATION_MZONE,1,nil)
+-- Activation: place counters
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	for tc in g:Iter() do
+		tc:AddCounter(COUNTER_THREAD,1)
 	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsControler,1-tp),tp,0,LOCATION_MZONE,1,1,nil)
 end
 
---Equip operation
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if not c:IsRelateToEffect(e) or not tc or not tc:IsRelateToEffect(e) then return end
-	Duel.Equip(tp,c,tc)
+-- Restriction target (monster has Thread Counter)
+function s.atktg(e,c)
+	return c:GetCounter(COUNTER_THREAD)>0
 end
 
---Equip limit
-function s.eqlimit(e,c)
-	return c:IsControler(1-e:GetHandlerPlayer())
+-- Opponent's End Phase condition
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==1-tp
 end
 
---ATK reduction
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local ec=c:GetEquipTarget()
-	if ec then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(-500)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		ec:RegisterEffect(e1)
+-- Damage operation
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local total=Duel.GetCounter(0,LOCATION_ONFIELD,LOCATION_ONFIELD,COUNTER_THREAD)
+	if total>0 then
+		Duel.Damage(1-tp,total*700,REASON_EFFECT)
 	end
+end
+
+-- Remove all Thread Counters when this leaves
+function s.remop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RemoveCounter(tp,LOCATION_ONFIELD,LOCATION_ONFIELD,COUNTER_THREAD,0xFFFF,REASON_EFFECT)
 end
