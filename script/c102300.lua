a584897f885efa6d6e2a25bebdc2b7ddd025e9ff
joemmoma2022 <110416415+joemmoma2022@users.noreply.github.com>
2026-02-16 16:12:04 -0300

    Update c102300.lua

--- script/c102300.lua
+++ script/c102300.lua
@@ -1,50 +1,34 @@
--- Equalizer Protocol
--- Auto-Start Spell (Self LP Only)
 local s,id=GetID()
 
-local SET_LP_SELF = 4000  -- LP you will be set to
+local SET_LP_SELF = 3000
 
 function s.initial_effect(c)
-	-- Startup trigger
+	-- Auto trigger Turn 1 Standby Phase
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetCode(EVENT_STARTUP)
-	e1:SetRange(LOCATION_ALL)
-	e1:SetCountLimit(1)
-	e1:SetOperation(s.startop)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_HAND+LOCATION_DECK)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 
-function s.startop(e,tp,eg,ep,ev,re,r,rp)
-	-- Fire at first draw phase (Turn 1) - ONCE
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PREDRAW)
-	e1:SetCondition(function(e,tp)
-		return Duel.GetCurrentChain()==0
-			and Duel.GetTurnCount()==1
-			and Duel.GetFlagEffect(tp,id)==0
-	end)
-	e1:SetOperation(s.activate)
-	e1:SetReset(RESET_PHASE|PHASE_DRAW)
-	Duel.RegisterEffect(e1,tp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()==1
+		and Duel.GetCurrentChain()==0
+		and Duel.GetFlagEffect(tp,id)==0
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 
-	-- Mark as used so it only resolves once
+	-- Ensure it only happens once
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
 
-	-- If this card started in hand, draw 1
-	if c:GetPreviousLocation()==LOCATION_HAND then
-		Duel.Draw(tp,1,REASON_EFFECT)
-	end
-
-	-- Set ONLY the activating player's LP
+	-- Set the activating player's LP
 	Duel.SetLP(tp,SET_LP_SELF)
 
-	-- Banish face-down after resolution
+	-- Banish this card face-down from wherever it is
 	Duel.Remove(c,POS_FACEDOWN,REASON_EFFECT)
 end
